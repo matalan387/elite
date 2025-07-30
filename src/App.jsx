e47e86b13b00e6dbeff1fe1ce379feaabcea91e5
matalan387 <matt.all93@gmail.com>
2025-07-30 22:03:02 +0100

    Update placeholder for cafe

--- src/App.jsx
+++ src/App.jsx
@@ -194,7 +194,7 @@ const GreasySpoonApp = () => {
                   value={newEvent.spoonName}
                   onChange={(e) => setNewEvent({...newEvent, spoonName: e.target.value})}
                   className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  placeholder="Tony's Cafe"
+                  placeholder="Gaz's Cafe"
                   disabled={submitting}
                 />
               </div>
